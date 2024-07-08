8a9f1916cc811c9cca1935ffc4fd9740249b7add
Umme-Nahida <mysuccessmission@gmail.com>
2024-07-08 14:13:08 +0600

    responsive btn created

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import CardProduct from "./CardProduct";
 import productBg from "../images/more/1.png"
+import { Link } from "react-router-dom";
 
 
 const Products = () => {
@@ -22,7 +23,17 @@ const Products = () => {
     }
     return (
         <div className="p-10 px-40 bg-cover " style={{backgroundImage: `url(${productBg})`}}>
-            this is product section
+            <div className="text-center space-y-3 mb-10">
+                <p>--- Sip & Savor ---</p>
+                <h1 className="text-2xl text-[#6F4E37] font-rancho pb-2">Our Popular Products</h1>
+                <Link to='/addCoffee'>
+                <button className="btn btn-sm md:btn-md bg-[#E3B577] text-white hover:bg-[#E3B577] border border-[#331A15]">
+                    Add Coffee
+                </button>
+                
+                </Link>
+            </div>
+
            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5 lg:gap-10">
            {
                 cofees?.map(coffee=>
